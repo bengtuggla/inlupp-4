da32075a0f671a36164d944538ea210753ccdb51
Bengt Uggla <bengt.uggla@gmail.com>
2021-02-22 13:54:15 +0100

    Fixed disabling saving empty string + enabling removing item ONLY IF DONE

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ const input = document.querySelector('#input')
 async function getTodosFromJP() {
  const response = await fetch('https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10');
  const data = await response.json();
- postInList(data)
-   console.log(data);
+ const newArray= Array.from(data);// Create a new array from jsonPlaceholder data
+ postInList(newArray)
+   console.log(newArray);
+   for(i=0;i<newArray.length; i++){
+    console.log(newArray[i]);
+   }
 }
 
 //Invoke get Todo-list from jsonPlaceholder
@@ -42,13 +46,23 @@ async function createTodo(e){
 
 // Insert Listitems with Todo-item + Delete-button
 function postInList(todoList){
+ 
 for(let i=0; i<todoList.length; i++ ){
 
+if(todoList[i].completed != true){
  myList.innerHTML += ` <li class="list-group-item d-flex justify-content-between">
  ${todoList[i].title}
 
   <button class="btn btn-danger btn-sm float-right delete">X</button>
   </li>`
+  
+  }else{
+    myList.innerHTML += ` <li class="list-group-item d-flex justify-content-between done">
+ ${todoList[i].title}
+
+  <button class="btn btn-danger btn-sm float-right delete">X</button>
+  </li>`
+  }
   
 }
 
@@ -61,23 +75,24 @@ function addItem(e) {
  e.preventDefault();
  let item = document.querySelector('#input').value;
 // Invoke Create Todo-Item
+if(item != ''){
  createTodo(item);
 // Empty form field
 document.querySelector('#input').value = ''
 insertNewItem(item)
+}else{
+ error.textContent = 'You must enter some text'
+}
 }
 
 // Insert new Todo + Deletebutton from Form as first Item
 function insertNewItem(_item){
- if(_item !== ''){
+ 
    myList.insertAdjacentHTML('afterbegin', ` <li class="list-group-item d-flex justify-content-between">
    ${_item}
   <button class="btn btn-danger btn-sm float-right delete ">X</button>
    </li>`)
-   }else{
-     error.textContent = 'You must enter some text'
-     //input.value="You must enter some text"
-   }
+   
 }
 
 // Eventlistener toggle for "Completed"
@@ -94,8 +109,13 @@ function removeItem(e){
  if(e.target.classList.contains('delete')){
   if(confirm('Are you shure?')){
    const li = e.target.parentElement;
-   myList.remove(li)
-   //location.reload()
+
+  if(li.classList.contains('done')){
+   myList.removeChild(li)
+   }else{
+    alert('Todo have to be clicked and Done for Removing')
+   }
   }
  }
 }
+
